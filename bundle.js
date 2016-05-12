5a3ec938dc3eaea5acf0ac2b90b789c6ba0d7c99
Freddy Roberts <freddyroberts9@gmail.com>
2016-05-12 00:08:04 -0500

    fix jquery bug

--- bundle.js
+++ bundle.js
@@ -1,60 +1,60 @@
 (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
-$( document ).ready(function() {
-  $('.web').addClass('animated fadeInDown');
-  $( ".background-color" ).animate({
-   opacity: 0.75,
-}, 4000, function() {});
-  $('.scroll-down').addClass('animated fadeIn');
+$(document).ready(function() {
+    $('.web').addClass('animated fadeInDown');
+    $(".background-color").animate({
+        opacity: 0.75,
+    }, 3000, function() {});
+    $('.scroll-down').addClass('animated fadeIn');
 });
 
-window.addEventListener("scroll", function(evt) {
+window.addEventListener("scroll", function(e) {
+    e.preventDefault();
     var pos_top = $(window).scrollTop();
-     if(pos_top >= 400){
-       $( ".header" ).fadeIn( "slow", function(){});
-   } else {
-      $( ".header" ).fadeOut( "fast", function(){});
-   }
+    if (pos_top >= 400) {
+        $(".header").fadeIn("slow", function() {});
+    } else {
+        $(".header").fadeOut("fast", function() {});
+    }
 });
 
-$( "#bottom" ).on( "click", function( event ) {
-  $("html, body").animate({
-    scrollTop: $("#top").offset().top
-  }, 1180);
+$("#bottom").on("click", function(e) {
+    e.preventDefault();
+    $("html, body").animate({
+        scrollTop: $("#top").offset().top
+    }, 1180);
 });
-$( ".scroll-down" ).on( "click", function() {
-  $("html, body").animate({
-    scrollTop: $("#contact").offset().top
-  }, 1480);
+$(".scroll-down").on("click", function(e) {
+    e.preventDefault();
+    $("html, body").animate({
+        scrollTop: $("#contact").offset().top
+    }, 1480);
     $('.scroll-down').addClass('animated fadeOut');
 });
 
-$( "#aboutLnk" ).on( "click", function( event ) {
-  $("html, body").animate({
-    scrollTop: $("#about").offset().top
-  }, 850);
+$("#aboutLnk").on("click", function(e) {
+    e.preventDefault();
+    $("html, body").animate({
+        scrollTop: $("#about").offset().top
+    }, 850);
 });
 
-$( "#skillsLnk" ).on( "click", function( event ) {
-  $("html, body").animate({
-    scrollTop: $("#skills").offset().top
-  }, 850);
+$("#skillsLnk").on("click", function(e) {
+    e.preventDefault();
+    $("html, body").animate({
+        scrollTop: $("#skills").offset().top
+    }, 850);
 });
-$( "#projectsLnk" ).on( "click", function( event ) {
-  $("html, body").animate({
-    scrollTop: $("#projects").offset().top
-  }, 850);
-});
-
-$( "#portfolioLnk" ).on( "click", function( event ) {
-  $("html, body").animate({
-    scrollTop: $("#portfolio").offset().top
-  }, 850);
+$("#projectsLnk").on("click", function(e) {
+    e.preventDefault();
+    $("html, body").animate({
+        scrollTop: $("#projects").offset().top
+    }, 850);
 });
 
-$( "#contactLnk" ).on( "click", function( event ) {
-  $("html, body").animate({
-    scrollTop: $("#contact").offset().top
-  }, 850);
+$("#contactLnk").on("click", function(e) {
+    e.preventDefault();
+    $("html, body").animate({
+        scrollTop: $("#contact").offset().top
+    }, 850);
 });
-
 },{}]},{},[1]);
--- source/js/main.js
+++ source/js/main.js
@@ -1,57 +1,58 @@
-$( document ).ready(function() {
-  $('.web').addClass('animated fadeInDown');
-  $( ".background-color" ).animate({
-   opacity: 0.75,
-}, 4000, function() {});
-  $('.scroll-down').addClass('animated fadeIn');
+$(document).ready(function() {
+    $('.web').addClass('animated fadeInDown');
+    $(".background-color").animate({
+        opacity: 0.75,
+    }, 3000, function() {});
+    $('.scroll-down').addClass('animated fadeIn');
 });
 
-window.addEventListener("scroll", function(evt) {
+window.addEventListener("scroll", function(e) {
+    e.preventDefault();
     var pos_top = $(window).scrollTop();
-     if(pos_top >= 400){
-       $( ".header" ).fadeIn( "slow", function(){});
-   } else {
-      $( ".header" ).fadeOut( "fast", function(){});
-   }
+    if (pos_top >= 400) {
+        $(".header").fadeIn("slow", function() {});
+    } else {
+        $(".header").fadeOut("fast", function() {});
+    }
 });
 
-$( "#bottom" ).on( "click", function( event ) {
-  $("html, body").animate({
-    scrollTop: $("#top").offset().top
-  }, 1180);
+$("#bottom").on("click", function(e) {
+    e.preventDefault();
+    $("html, body").animate({
+        scrollTop: $("#top").offset().top
+    }, 1180);
 });
-$( ".scroll-down" ).on( "click", function() {
-  $("html, body").animate({
-    scrollTop: $("#contact").offset().top
-  }, 1480);
+$(".scroll-down").on("click", function(e) {
+    e.preventDefault();
+    $("html, body").animate({
+        scrollTop: $("#contact").offset().top
+    }, 1480);
     $('.scroll-down').addClass('animated fadeOut');
 });
 
-$( "#aboutLnk" ).on( "click", function( event ) {
-  $("html, body").animate({
-    scrollTop: $("#about").offset().top
-  }, 850);
+$("#aboutLnk").on("click", function(e) {
+    e.preventDefault();
+    $("html, body").animate({
+        scrollTop: $("#about").offset().top
+    }, 850);
 });
 
-$( "#skillsLnk" ).on( "click", function( event ) {
-  $("html, body").animate({
-    scrollTop: $("#skills").offset().top
-  }, 850);
+$("#skillsLnk").on("click", function(e) {
+    e.preventDefault();
+    $("html, body").animate({
+        scrollTop: $("#skills").offset().top
+    }, 850);
 });
-$( "#projectsLnk" ).on( "click", function( event ) {
-  $("html, body").animate({
-    scrollTop: $("#projects").offset().top
-  }, 850);
-});
-
-$( "#portfolioLnk" ).on( "click", function( event ) {
-  $("html, body").animate({
-    scrollTop: $("#portfolio").offset().top
-  }, 850);
+$("#projectsLnk").on("click", function(e) {
+    e.preventDefault();
+    $("html, body").animate({
+        scrollTop: $("#projects").offset().top
+    }, 850);
 });
 
-$( "#contactLnk" ).on( "click", function( event ) {
-  $("html, body").animate({
-    scrollTop: $("#contact").offset().top
-  }, 850);
+$("#contactLnk").on("click", function(e) {
+    e.preventDefault();
+    $("html, body").animate({
+        scrollTop: $("#contact").offset().top
+    }, 850);
 });
