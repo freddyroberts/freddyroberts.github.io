39975a891559b599573a11eb18870325c5a8baa0
Freddy Roberts <freddyroberts9@gmail.com>
2016-05-12 16:10:36 -0500

    fix scroll to id bug

--- source/js/main.js
+++ source/js/main.js
@@ -21,39 +21,39 @@ window.addEventListener("scroll", function(e) {
 $("#bottom").on("click", function(e) {
     e.preventDefault();
     $("html, body").animate({
-        scrollTop: $("#top").offset().top
+        scrollTop: ($("#top").offset().top - 70)
     }, 1180);
 });
 $(".scroll-down").on("click", function(e) {
     e.preventDefault();
     $("html, body").animate({
-        scrollTop: $("#contact").offset().top
+        scrollTop: ($("#contact").offset().top - 70)
     }, 1480);
 });
 
 $("#aboutLnk").on("click", function(e) {
     e.preventDefault();
     $("html, body").animate({
-        scrollTop: $("#about").offset().top
+        scrollTop: ($("#about").offset().top - 70)
     }, 850);
 });
 
 $("#skillsLnk").on("click", function(e) {
     e.preventDefault();
     $("html, body").animate({
-        scrollTop: $("#skills").offset().top
+        scrollTop: ($("#skills").offset().top - 70)
     }, 850);
 });
 $("#projectsLnk").on("click", function(e) {
     e.preventDefault();
     $("html, body").animate({
-        scrollTop: $("#projects").offset().top
+        scrollTop: ($("#projects").offset().top - 70)
     }, 850);
 });
 
 $("#contactLnk").on("click", function(e) {
     e.preventDefault();
-    $("html, body").animate({
-        scrollTop: $("#contact").offset().top
-    }, 850);
+    $('html, body').animate({
+    scrollTop: ($( $.attr(this, 'href') ).offset().top - 70)
+  }, 850);
 });
